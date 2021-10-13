d07023a401f381eadfdaed0a1fb1ba00eb9fc4ef
erkobridee <erkobridee@users.noreply.github.com>
2021-10-13 08:57:22 +0000

    deploy: aa3524a296928e84f0a92c18bd8d498d8b651a31

---
+++ _next/static/lUxDbTsPUpdJyRLi2F7Rx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/22108a45c2662a42c9dd.css","static/chunks/pages/index-0d797fd1ac20a84d290b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
