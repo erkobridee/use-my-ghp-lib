e92fe2584e6e37e01256cfb8e56f8581c0305d76
Erko Bridee <erko.bridee@gmail.com>
2021-10-13 10:56:06 +0200

    update(next): config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  basePath: '/use-my-ghp-lib',
-};
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {};
+
+if (`${process.env.NODE_ENV}` === 'production') {
+  nextConfig.basePath = '/use-my-ghp-lib';
+}
+
+module.exports = nextConfig;
